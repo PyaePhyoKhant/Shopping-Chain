4166802f198897b9f4008d8043739d7de0ea8576
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2018-02-22 23:13:14 +0630

    add Block model (for now, no ImageField)

--- blockchain/models.py
+++ blockchain/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 import string
 import random
+import hashlib
+from django.utils import timezone
+
+TO_SELL = 'to sell'
+SOLD = 'sold'
 
 
 class Shopper(models.Model):
@@ -16,3 +21,26 @@ class Shopper(models.Model):
             password = ''.join(random.choice(chars) for _ in range(8))
             self.user = User.objects.create_user(self.name, email, password)
         super(Shopper, self).save(*args, **kwargs)
+
+
+def hash_block(block_instance):
+    return hashlib.sha256(str(block_instance.id).encode('utf-8') +
+                          str(block_instance.title).encode('utf-8') +
+                          str(block_instance.created_at).encode('utf-8') +
+                          str(block_instance.price).encode('utf-8') +
+                          str(block_instance.seller).encode('utf-8') +
+                          str(block_instance.status).encode('utf-8') +
+                          str(block_instance.prev_id).encode('utf-8') +
+                          str(block_instance.prev_hash).encode('utf-8')).digest()
+
+
+class Block(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
+    price = models.IntegerField()
+    seller = models.ForeignKey(Shopper, related_name='seller', blank=True, null=True)
+    buyer = models.ForeignKey(Shopper, related_name='buyer', blank=True, null=True)
+
+    status = models.CharField(max_length=100)
+    prev_id = models.IntegerField(blank=True, null=True)
+    prev_hash = models.BinaryField(blank=True, null=True)
--- blockchain/test_factories.py
+++ blockchain/test_factories.py
@@ -1,5 +1,5 @@
 import factory
-from blockchain.models import Shopper
+from blockchain.models import Shopper, Block, TO_SELL
 import random
 
 
@@ -9,3 +9,14 @@ class ShopperFactory(factory.django.DjangoModelFactory):
 
     name = factory.Faker('name')
     phone = '09' + ''.join([random.choice('0123456789') for _ in range(9)])
+
+
+class BlockFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Block
+
+    title = random.choice(['bag', 'cat', 'dog', 'computer', 'phone'])
+    price = random.randrange(1000, 100000, 100)
+    seller = factory.SubFactory(ShopperFactory)
+    buyer = factory.SubFactory(ShopperFactory)
+    status = TO_SELL
